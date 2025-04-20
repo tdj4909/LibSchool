460546f64fc325290ce9c7ec19f3379d5d62f74a
tdj4909 <tdj4909@gmail.com>
2025-04-20 22:02:08 +0900

    start

---
+++ src/main/java/com/papasong/LibSchool/LibSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.papasong.LibSchool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LibSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/papasong/LibSchool/LibSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papasong.LibSchool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
