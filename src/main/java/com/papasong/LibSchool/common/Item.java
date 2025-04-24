c0ef1954b188585e7c849d145ba055677540e094
tdj4909 <tdj4909@gmail.com>
2025-04-24 23:02:41 +0900

    update

---
+++ src/main/java/com/papasong/LibSchool/common/Item.java
@@ -0,0 +1,16 @@
+package com.papasong.LibSchool.common;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Item {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    public Long id;
+
+    public String title;
+    public Integer price;
+}
